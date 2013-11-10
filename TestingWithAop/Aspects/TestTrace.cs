335e246bab4697e1e0f6bbaadcf72aa062a474ac
NimrodTicotzner <nimrod.ticozner@gmail.com>
2013-11-10 12:23:23 +0200

    Push solution to repo

---
+++ TestingWithAop/Aspects/TestTrace.cs
@@ -0,0 +1,35 @@
+﻿using PostSharp.Aspects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingWithAop.Aspects
+{
+    /// <summary>
+    /// Aspect sttribute class for logging phases of test methods
+    /// </summary>
+    [Serializable]
+    public class TestTrace : OnMethodBoundaryAspect
+    {
+
+        public sealed override void OnEntry(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Entrying Test Method : " + args.Method.Name);
+        }
+
+        public sealed override void OnExit(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Exiting Test Method : " + args.Method.Name);
+        }
+
+        public sealed override void OnException(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Exception in Test : " + args.Method.Name);
+            Console.WriteLine("Message : " + args.Exception.Message);
+            Console.WriteLine("Stack Trace : " + args.Exception.StackTrace);
+        }
+
+    }
+}
---
+++ TestingWithAop/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingWithAop.Aspects;
+
+namespace TestingWithAop
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestTrace]
+        public void TestMethod1()
+        {
+            Console.WriteLine("This is the first test.");
+            throw new Exception("Dummy exception");
+        }
+    }
+}
